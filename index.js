523f284bde76af02dec893f9143d3a41c37c4ee6
Shreya Mishra <shreyamishra2k05@gmail.com>
2025-07-31 16:28:12 +0530

    Weather App

---
+++ index.js
@@ -0,0 +1,55 @@
+const input = document.getElementById('dropdown');
+
+const cityName = document.querySelector('.location');
+const humidity = document.querySelector('.humid');
+const windSpeed = document.querySelector('.wind');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const image = document.querySelector('.weather-icon');
+
+const text = document.querySelector('.city-name');
+const button = document.querySelector('button');
+
+const API_KEY = '623878137ab28bb1f733637354e80ede';
+
+
+function showData() {
+ const val = input.value.toLowerCase();
+ const url = `https://api.openweathermap.org/data/2.5/weather?q=${val}&appid=${API_KEY}&units=metric`;
+  if(val){
+    fetchWeather(url);
+    function fetchWeather(url) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            text.innerText = data.name;
+            temperature.innerText = `${Math.round(data.main.temp)}°C`;
+            description.innerText = data.weather[0].description;
+            windSpeed.innerText = ` ${data.wind.speed} km/hr`;
+            humidity.innerText = ` ${data.main.humidity}%`;
+            image.innerHTML = data.weather[0].icon.png;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+      }
+    }
+};
+
+button.addEventListener('click', (evt) => 
+  { 
+    evt.preventDefault();
+    {if(input.value === '')
+        text.innerText = 'Cuttack';
+    else
+      showData();
+    }
+  }
+);
+
+input.addEventListener('keypress', (evt) => {
+if(evt.key === 'Enter') {
+  evt.preventDefault();
+  showData();
+}
+});
